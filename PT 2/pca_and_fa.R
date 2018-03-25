31faff20092a53a09bb133dc7898747dc5168264
et4 <profour@mail.ru>
2018-03-26 00:00:03 +0300

    fd

--- PT 2/pca_and_fa.R
+++ PT 2/pca_and_fa.R
@@ -4,4 +4,22 @@ x <- read.csv(file = "D:/F0UR/Projects/Statistic/stat/PT 2/normalized_school_dat
 
 #plot(x)
 
-stick.fa6 = factanal(x, factors=6, method = "mls", scores = "regression")
+#stick.fa6 = factanal(x, factors=6, method = "mls", scores = "regression")
+
+fit <- princomp(x, cor=TRUE)
+summary(fit) # print variance accounted for 
+loadings(fit) # pc loadings 
+plot(fit,type="lines") # scree plot 
+fit$scores # the principal components
+biplot(fit)
+
+library(psych)
+fit <- principal(x, nfactors=6, rotate="varimax")
+fit # print results
+
+fit <- factanal(x, 6, rotation="varimax")
+print(fit, digits=2, cutoff=.3, sort=TRUE)
+# plot factor 1 by factor 2 
+load <- fit$loadings[,1:2] 
+plot(load,type="n") # set up plot 
+text(load,labels=names(mydata),cex=.7) # add variable names
